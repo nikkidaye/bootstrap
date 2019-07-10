a66d02e361eeb89d840dd77351e5ad1fe07e369c
Nikki Daye <nikkidaye@gmail.com>
2019-07-10 11:48:52 -0400

    created express server

---
+++ index.js
@@ -0,0 +1,14 @@
+const express =  require('express');
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Example app listening on port ${PORT}!`);
+});
+
+app.use(express.static(__dirname + '/Public'));
+
+app.get('/', (request, response) => {
+  response.sendFile(__dirname + '/Public/index.html');
+});
